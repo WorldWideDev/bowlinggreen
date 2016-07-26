86f0fcd58904d8b2c34b42558e77adcbdbcf821e
WorldWideDev <dev206@gmail.com>
2016-07-26 14:53:47 -0700

    scrollTo now aligns with div height, added img carosel

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
      $('#logo-container').click(function(){
-        $('html,body').animate({scrollTop: $('#top').offset().top}, 'slow');
+        $('html,body').animate({scrollTop: $('#welcome').offset().top - $('#welcome').height()}, 'slow');
         return false
     })
-    $('#scroll2section1').click(function(){
-        $('html,body').animate({scrollTop: $('#section1').offset().top}, 'slow');
+    $('.scroll2section1').click(function(){
+        $('html,body').animate({scrollTop: $('#section1').offset().top - $('.scroll2section1').height()}, 'slow');
         return false
     })
-    $('#scroll2section2').click(function(){
-        $('html,body').animate({scrollTop: $('#section2').offset().top}, 'slow');
+    $('.scroll2section2').click(function(){
+        $('html,body').animate({scrollTop: $('#section2').offset().top - $('.scroll2section2').height()}, 'slow');
         return false
     })
-    $('#scroll2section3').click(function(){
-        $('html,body').animate({scrollTop: $('#section3').offset().top}, 'slow');
+    $('.scroll2section3').click(function(){
+        $('html,body').animate({scrollTop: $('#section3').offset().top - $('.scroll2section3').height()}, 'slow');
         return false
     })
-     $('#scroll2section4').click(function(){
-        $('html,body').animate({scrollTop: $('#section4').offset().top}, 'slow');
+     $('.scroll2section4').click(function(){
+        $('html,body').animate({scrollTop: $('#section4').offset().top - $('.scroll2section4').height()}, 'slow');
         return false
     })
+    $('.slider').slider({full_width: true});
 })
